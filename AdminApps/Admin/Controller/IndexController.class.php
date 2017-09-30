a3a1b5ba2f1b400356c498fd11caee3b2d37cc3e
caraduu <1678404908@qq.com>
2017-09-30 10:05:04 +0800

    默认的

--- AdminApps/Admin/Controller/IndexController.class.php
+++ AdminApps/Admin/Controller/IndexController.class.php
@@ -39,7 +39,7 @@ class IndexController extends SuperController {
         $this->assign('resources', $resources);
         $this->display('Index/index');
     }
-//根据ip获取当前所在位置htyhththt
+//根据ip获取当前所在位置hj
     public function maps(){
         $addr=I('addr');
         $file="http://apis.map.qq.com/ws/location/v1/ip?&key=AYTBZ-ZREKJ-ATVF3-FWMEW-FFXC5-CVF5Y";//根据端ip获取所在位置
